d49a52511c3c238f4bccd7a82118a5ddc9c67cac
Andrei Manz <andrei.manz@gmail.com>
2026-02-18 12:26:14 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/components/medishelf/sidebar.tsx
+++ src/components/medishelf/sidebar.tsx
@@ -25,12 +25,13 @@ export function Sidebar() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-        <div
+        <Link
+          href="/"
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
           <LogoIcon className="h-6 w-6" />
           <span className="sr-only">MediShelf</span>
-        </div>
+        </Link>
         <TooltipProvider>
           {navItems.map((item) => (
             <Tooltip key={item.href}>
