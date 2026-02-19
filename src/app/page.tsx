fb4a175ae0f70cf130f17212a90605f0169c4e77
Andrei Manz <andrei.manz@gmail.com>
2026-02-19 14:32:36 +0000

    the label is black and I can't see it!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,13 +133,13 @@ export default function Home() {
               <BarChart data={chartData}>
                 <XAxis
                   dataKey="name"
-                  stroke="#888888"
+                  stroke="hsl(var(--muted-foreground))"
                   fontSize={12}
                   tickLine={false}
                   axisLine={false}
                 />
                 <YAxis
-                  stroke="#888888"
+                  stroke="hsl(var(--muted-foreground))"
                   fontSize={12}
                   tickLine={false}
                   axisLine={false}
