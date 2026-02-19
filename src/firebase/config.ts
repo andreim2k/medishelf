56c8a1183b1a4ead3633a3e02b3768ed358e190e
Andrei Manz <andrei.manz@gmail.com>
2026-02-19 16:38:33 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-// This file holds the configuration for your Firebase project.
-// Replace the placeholder values with your actual Firebase project credentials.
-// You can get these from the Firebase console for your web app.
 export const firebaseConfig = {
-  apiKey: "AIzaSyCZV-e3-IunnMr19KNJw17oj73y7u5IPK8",
-  authDomain: "studio-5969350944-8e8f1.firebaseapp.com",
-  projectId: "studio-5969350944-8e8f1",
-  storageBucket: "studio-5969350944-8e8f1.appspot.com",
-  messagingSenderId: "908694561679",
-  appId: "1:908694561679:web:251b9fb39db71210f8e1fa",
-  measurementId: ""
+  "projectId": "studio-5969350944-8e8f1",
+  "appId": "1:908694561679:web:251b9fb39db71210f8e1fa",
+  "apiKey": "AIzaSyCZV-e3-IunnMr19KNJw17oj73y7u5IPK8",
+  "authDomain": "studio-5969350944-8e8f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "908694561679"
 };
